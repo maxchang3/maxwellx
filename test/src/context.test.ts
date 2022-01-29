e2726b43ddc9fe46e58fbf9df15665ddc3dad2db
MaxChang3 <maxcfoo@gmail.com>
2022-01-29 21:28:22 +0800

    test(test:context): move test file path

--- test/src/context.test.ts
+++ test/src/context.test.ts
@@ -3,5 +3,5 @@ import { readConfig, readPostContext} from "@maxwell-blog/context";
 let config = await readConfig()
 console.log(config)
 
-let post = await readPostContext("_posts","testpost.md");
+let post = await readPostContext("source/_posts","testpost.md");
 console.log(post.frontMatter)
