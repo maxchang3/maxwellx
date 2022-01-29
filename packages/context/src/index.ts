b20fac53f8fb10b18208ee2952e60890a97b2735
MaxChang3 <maxcfoo@gmail.com>
2022-01-29 20:23:13 +0800

    fix(context): split func to mult files

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -1,25 +1,3 @@
-import path from "path";
-import { promises as fs } from "fs";
-import { readFileOptions } from "./types";
-
-const __dirname = process.cwd();
-
-const configPath = path.join(__dirname, 'maxwell.config.js')
-
-function getFilePath(folder: string, filename: string) {
-    return path.join(__dirname, folder, filename)
-}
-
-async function readFileContent(folder: string, filename: string, options?: readFileOptions) {
-    let fileContent = await fs.readFile(getFilePath(folder, filename), {encoding: options?.encoding || 'utf-8'});
-    if(options?.escape) return // todo ;
-    return fileContent
-}
-
-async function readConfig() {
-    let config = (await import(configPath)).default
-    return config
-}
-
-
-export { readConfig, readFileContent }
+export { readConfig } from './config.js'
+export { parseFrontMatter, readPostContext } from './post.js'
+export { readFileContent, getFilePath, configPath } from './utils.js'
