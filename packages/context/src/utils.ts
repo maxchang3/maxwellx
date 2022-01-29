6fa72474926853291c23a9de72ab6328bd4a5e7d
MaxChang3 <maxcfoo@gmail.com>
2022-01-29 20:22:51 +0800

    feat(context: add utils):

---
+++ packages/context/src/utils.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { promises as fs } from "fs";
+import type { readFileOptions } from "./types";
+
+const __dirname = process.cwd();
+
+const configPath = path.join(__dirname, 'maxwell.config.js')
+
+function getFilePath(folder: string, filename: string) {
+    return path.join(__dirname, folder, filename)
+}
+
+async function readFileContent(folder: string, filename: string, options?: readFileOptions) {
+    let fileContent = await fs.readFile(getFilePath(folder, filename), { encoding: options?.encoding || 'utf-8' });
+    if (options?.escape) return fileContent// todo ;
+    return fileContent
+}
+
+export { readFileContent, getFilePath ,configPath}
