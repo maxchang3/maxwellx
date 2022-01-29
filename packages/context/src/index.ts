ff22252bd92e3189d9a9b87b4d887e013b4c7ab6
MaxChang3 <maxcfoo@gmail.com>
2022-01-29 19:19:37 +0800

    feat: add universal read func

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -1,12 +1,19 @@
-import path  from "path";
-import { promises as fs}  from "fs";
+import path from "path";
+import { promises as fs } from "fs";
+import { readFileOptions } from "./types";
 
 const __dirname = process.cwd();
 
 const configPath = path.join(__dirname, 'maxwell.config.js')
 
-async function readFileContent(filenpath:string) {
-    return await fs.readFile('/etc/passwd');
+function getFilePath(folder: string, filename: string) {
+    return path.join(__dirname, folder, filename)
+}
+
+async function readFileContent(folder: string, filename: string, options?: readFileOptions) {
+    let fileContent = await fs.readFile(getFilePath(folder, filename), {encoding: options?.encoding || 'utf-8'});
+    if(options?.escape) return // todo ;
+    return fileContent
 }
 
 async function readConfig() {
@@ -14,7 +21,5 @@ async function readConfig() {
     return config
 }
 
-async function readMarkdownFrontMatter(){
 
-}
-export {readConfig,readFileContent}
+export { readConfig, readFileContent }
