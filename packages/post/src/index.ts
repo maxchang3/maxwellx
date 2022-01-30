b1f5821d9ce429195bae3ba379d61bdea23661b4
MaxChang3 <maxcfoo@gmail.com>
2022-01-31 00:05:07 +0800

    feat(post): add post package

---
+++ packages/post/src/index.ts
@@ -0,0 +1,12 @@
+import { context,readPostContext} from "@maxwellx/context"
+import { getFiles } from "@maxwellx/context"
+
+async function* getPostFilesContent(context: context) {
+    const basepath = [context.config.directory.source,"_posts"]
+    const files = getFiles(...basepath)
+    for await(let file of files){
+         yield readPostContext(basepath,file)
+    }
+}
+
+export { getPostFilesContent }
