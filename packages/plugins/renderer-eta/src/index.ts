d31e38730cad38615d61463133dfe8883c5e80a8
MaxChang3 <maxcfoo@gmail.com>
2022-01-30 21:18:45 +0800

    fix(plugin/renderer-eta): change path logical

--- packages/plugins/renderer-eta/src/index.ts
+++ packages/plugins/renderer-eta/src/index.ts
@@ -1,15 +1,12 @@
 import { __dirname } from "@maxwell-blog/context"
 import { Renderer } from "@maxwell-blog/renderer";
-import path from "path"
 import {renderFile,configure} from "eta"
 
-configure({
-    views: path.join(__dirname, "theme")
-})
-
-
-const etaRenderer = new Renderer("eta","html","theme",async(data,context,options)=>{
-    return renderFile(data.content, context) as Promise<string>;
+const etaRenderer = new Renderer("eta","html",async(data,context,options)=>{
+    configure({
+        views: data.paths
+    })
+    return renderFile(data.filename, context) as Promise<string>;
 })
 
 export {etaRenderer}
