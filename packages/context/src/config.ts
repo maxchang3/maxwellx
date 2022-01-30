57a2fe7de0d872bfefaf0e4c9eb070ea1d488fa4
MaxChang3 <maxcfoo@gmail.com>
2022-01-30 14:54:44 +0800

    feat(context): add config types

--- packages/context/src/config.ts
+++ packages/context/src/config.ts
@@ -1,8 +1,11 @@
 import { configPath } from "./utils.js";
-
+import { configure } from "./types";
 async function readConfig() {
     let config = (await import(configPath)).default
     return config
 }
 
-export { readConfig }
+function returnConfig(config:configure):configure {
+    return config
+}
+export { readConfig,returnConfig}
--- packages/context/src/types.ts
+++ packages/context/src/types.ts
@@ -9,4 +9,35 @@ interface postContext {
     frontMatter: frontMatter
     content: string
 }
-export { readFileOptions, postContext, frontMatter }
+interface configure{
+    /** site information */
+    site: {
+        /** site title */
+        title: string,
+        /** the author's name will show in the post */
+        author: string,
+    },
+    /** site url configure */
+    url: {
+        /** site url such as https://example.com */
+        url: string,
+        /** site root path, if your site in a subdirectory such as `/blog/` just set it to `blog`   */
+        root: string,
+        /** site url route link for more information: https://todo.co/ */
+        permalink: string
+    },
+    /** site directory */
+    directory:{
+        /** source file (mostly markdown) directroty */
+        source: string,
+        /** public dir to generate to */
+        public: string,
+        /** template dir */
+        template: string
+    },
+    /** template name */
+    template: string,
+    /** plugins */
+    plugins: string[]
+}
+export { readFileOptions, postContext, frontMatter,configure }
