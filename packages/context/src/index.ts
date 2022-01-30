7dab184d93564ae9f053703df69b1d6b7e3349ac
MaxChang3 <maxcfoo@gmail.com>
2022-01-30 22:48:50 +0800

    feat(context-utils): add readFile func

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -1,4 +1,4 @@
 export { readConfig, returnConfig ,defaultConfig} from './config.js'
 export { parseFrontMatter, readPostContext } from './post.js'
-export { readFileContent, getFilePath, configPath, __dirname } from './utils.js'
+export { readFileContent, getFilePath, getFiles,  configPath, __dirname } from './utils.js'
 export type { context,configure, postContext } from './types'
--- packages/context/src/utils.ts
+++ packages/context/src/utils.ts
@@ -9,6 +9,19 @@ const configPath = path.join(__dirname, 'maxwell.config.js')
 function getFilePath(folder: string[], filename?: string) {
     return path.join(__dirname, ...folder, filename ? filename : '')
 }
+/** 
+遍历指定目录下文件与目录，并判断是否为文件。
+由于每次 Promise 为真，在 filter 中无法直接获取 stat.isDirectory。
+这里先使用 map 获取所有 Promise，再用 Promise.all 接收后 await。
+考虑到后期拓展性与性能优化，这里不使用同步版本的 fs。
+*/
+async function getFiles(...folder: string[]) {
+    let files = await fs.readdir(getFilePath(folder))
+    const fileStatPromise = files.map(file => fs.lstat(getFilePath(folder, file)))
+    const fileStat = await Promise.all(fileStatPromise)
+    files = files.filter((_file, index) => !(fileStat[index].isDirectory()))
+    return files
+}
 
 async function readFileContent(folder: string[], filename: string, options?: readFileOptions) {
     let fileContent = await fs.readFile(getFilePath(folder, filename), { encoding: options?.encoding || 'utf-8' });
@@ -16,4 +29,4 @@ async function readFileContent(folder: string[], filename: string, options?: rea
     return fileContent
 }
 
-export { readFileContent, getFilePath, configPath, __dirname, path }
+export { readFileContent, getFilePath, getFiles, configPath, __dirname, path }
