70f723a7905a73e74caf04785f351376db30ffa2
MaxChang3 <maxcfoo@gmail.com>
2022-01-30 21:28:10 +0800

    style(cofe): style

--- packages/core/src/maxwell.ts
+++ packages/core/src/maxwell.ts
@@ -1,14 +1,14 @@
-import {  context, readConfig,defaultConfig  } from "@maxwell-blog/context";
+import { context, readConfig, defaultConfig } from "@maxwell-blog/context";
 import { RendererList } from "@maxwell-blog/renderer";
 import { etaRenderer } from "@maxwell-blog/renderer-eta";
-import type {  maxwellCore } from './types'
+import type { maxwellCore } from './types'
 
 class maxwell implements maxwellCore {
     context: context;
     rendererList: RendererList;
     plugins: any;
     constructor() {
-        this.context = {config:defaultConfig};
+        this.context = { config: defaultConfig };
         this.rendererList = new RendererList(this.context);
     }
     async init() {
@@ -23,11 +23,11 @@ class maxwell implements maxwellCore {
     async setRenderer() {
         this.rendererList = new RendererList(this.context);
         this.rendererList.register(etaRenderer)
-        console.log(await etaRenderer.render([this.context.config.directory.template],"test",this.context))
+        console.log(await etaRenderer.render([this.context.config.directory.template], "test", this.context))
         // for plugins renderer
         // this.rendererList.register(all)
     }
-    async render(){
+    async render() {
         this.rendererList.render();
     }
 
