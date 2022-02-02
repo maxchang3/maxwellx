3a0911995ed9f520ef50fb19302614d36d539a2b
MaxChang3 <maxcfoo@gmail.com>
2022-02-02 22:13:56 +0800

    style(renderer): imporve style

--- packages/renderer/src/renderer.ts
+++ packages/renderer/src/renderer.ts
@@ -1,4 +1,9 @@
-import type { maxRenderer, renderOptions, withContent, renderData, withReading, renderFilepath } from "./types";
+import type {
+    maxRenderer, renderOptions, withContent,
+    renderData, withReading, renderFilepath,
+    dataOrPath
+} from "./types";
+
 import type { context } from '@maxwellx/context'
 import { getFilePath } from "@maxwellx/context";
 
@@ -11,15 +16,15 @@ export class Renderer<T extends (withContent | withReading)> implements maxRende
     constructor(callback: T, options?: renderOptions) {
         [this.callback, this.options] = [callback, options]
     }
-    render(data: (T extends withContent ? renderData : renderFilepath), context: context): Promise<string> {
-        if(this.options){
+    render(data: dataOrPath<T>, context: context): Promise<string> {
+        if (this.options) {
             let _data = (<renderFilepath>data)
             return (<withReading>this.callback)({
                 path: getFilePath([_data.path]),
                 filename: `${_data.filename}.${this.options.input}`
             }, context)
-        }else{
-            return (<withContent>this.callback)(<renderData>data, context)  
+        } else {
+            return (<withContent>this.callback)(<renderData>data, context)
         }
     }
 
--- packages/renderer/src/types.ts
+++ packages/renderer/src/types.ts
@@ -10,7 +10,7 @@ interface renderData {
 
 type withReading = (data: renderFilepath, context: context, options?: object) => Promise<string>;
 type withContent = (data: renderData, context: context, options?: object) => Promise<string>;
-
+type dataOrPath<T> = (T extends withContent ? renderData : renderFilepath)
 
 interface rendererWithRead {
     input: string;
@@ -18,15 +18,22 @@ interface rendererWithRead {
     callback: withReading;
     render(path: string[], filename: string, context: context, options?: object): Promise<string>
 }
+
 interface renderOptions {
     input: string;
     output: string;
 }
+
 interface maxRenderer<T extends (withContent | withReading)> {
     callback: T;
     options?: renderOptions;
-    render(data: (T extends withContent ? renderData : renderFilepath), context: context, options?: object): Promise<string>
+    render(data: dataOrPath<T>, context: context, options?: object): Promise<string>
 }
 
 
-export { renderFilepath, renderData, maxRenderer, rendererWithRead, withContent, withReading, renderOptions }
+export {
+    renderFilepath, renderData,
+    maxRenderer, rendererWithRead,
+    withContent, withReading,
+    renderOptions, dataOrPath
+}
