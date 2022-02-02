b1fa010b62398820af6404ffe8e99d468788dfd7
MaxChang3 <maxcfoo@gmail.com>
2022-02-02 23:17:36 +0800

    feat(core): import renderer from config

--- packages/core/src/maxwell.ts
+++ packages/core/src/maxwell.ts
@@ -1,33 +1,48 @@
 import { context, readConfig, defaultConfig } from "@maxwellx/context";
-import templateRenderer from "@maxwellx/renderer-eta";
-import markdownRenderer from "@maxwellx/renderer-markdown-it";
+import type{ Renderer, withContent, withReading } from "@maxwellx/api";
 import { getPostFilesContent } from "@maxwellx/post";
+import etaRenderer from "@maxwellx/renderer-eta";
+import markdownItRenderer from "@maxwellx/renderer-markdown-it";
 
 import type { maxwellCore } from './types'
 
 class maxwell implements maxwellCore {
     context: context;
+    renderer: {
+        template: Renderer<withReading>,
+        markdown: Renderer<withContent>
+    };
     plugins: any;
     constructor() {
         this.context = { config: defaultConfig };
+        this.renderer = {
+            template: etaRenderer,
+            markdown: markdownItRenderer
+        }
     }
     async init() {
         await this.setConfig();
-        //console.log(this.context)
+        let [_template , _markdown ] = await Promise.all([
+             import(this.context.config.renderer.template), 
+             import(this.context.config.renderer.markdown)
+            ])
+        console.log(_template,_markdown)
+        this.renderer  = {
+            template: _template.default,
+            markdown: _markdown.default
+        }
     }
     async setConfig() {
         this.context.config = await readConfig()
-       // console.log(this.context.config)
     }
     async render() {
-        // this.rendererList.render();
         for await(let content of getPostFilesContent(this.context)){
-            content.content = await markdownRenderer.render(content,this.context)
+            content.content = await this.renderer.markdown.render(content,this.context)
             let _context:context = {
                 config:this.context.config,
                 postContext: content
             }
-            let result = await templateRenderer.render({
+            let result = await this.renderer.template.render({
                 filename:content.frontMatter.layout,
                 path:this.context.config.directory.template            
             }, _context)
--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,7 +1,13 @@
 import type { context } from '@maxwellx/context'
+import type{ Renderer, withContent, withReading } from "@maxwellx/api";
+
 
 interface maxwellCore {
     context:context,
+    renderer: {
+        template: Renderer<withReading>,
+        markdown: Renderer<withContent>
+    },
     plugins: any,
 }
 
