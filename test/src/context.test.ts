62559595d5866fcbb0e04a73d877e109a8f9d435
MaxChang3 <maxcfoo@gmail.com>
2022-02-03 14:35:41 +0800

    test(test:context): change param input rules

--- test/src/context.test.ts
+++ test/src/context.test.ts
@@ -2,5 +2,5 @@ import { readConfig } from "@maxwellx/context";
 import { readPostContext } from '@maxwellx/post'
 
 
-let post = await readPostContext(["source", "_posts"], "testpost.md");
+let post = await readPostContext("source", "_posts", "testpost.md");
 console.log(post.frontMatter)
