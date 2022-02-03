da5b1bd53993c43438ca689ccc9979296c9e4b57
MaxChang3 <maxcfoo@gmail.com>
2022-02-03 15:45:48 +0800

    feat(api): add export type `PluginType`

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,3 +3,5 @@ export { definePlugin, getPluginPath , loadPluginModule } from './plugin.js'
 export { Renderer } from './renderer/index.js'
 
 export type { withContent, withReading } from './renderer/index.js'
+
+export type { PluginType } from './types'
