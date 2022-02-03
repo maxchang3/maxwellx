746d6cd9cb2cbd18cdb0957aef0f131e5bb55583
MaxChang3 <maxcfoo@gmail.com>
2022-02-03 14:41:27 +0800

    test: remove other tests

--- test/src/context.test.ts
+++
@@ -1,6 +0,0 @@
-import { readConfig } from "@maxwellx/context";
-import { getPostContext } from '@maxwellx/post'
-
-
-let post = await getPostContext("source", "_posts", "testpost.md");
-console.log(post.frontMatter)
