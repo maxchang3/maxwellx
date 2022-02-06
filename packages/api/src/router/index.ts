08ff98ffd83dfb63387fab699719e6cfba711279
MaxChang3 <maxcfoo@gmail.com>
2022-02-06 23:13:16 +0800

    fix(router): fix router logical

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -7,7 +7,6 @@ export class Router {
     #rule: string;
     #layoutContext: layoutContext;
     #routerKeywords: slugValues;
-    #slugInRule: string[] = [];
     #withIndex: boolean;
     #staticKeywords: string[] = [":year", ":month", ":day"]
 
@@ -27,27 +26,40 @@ export class Router {
         ":title": this.#layoutContext.frontMatter.title,
         ":layout": this.#layoutContext.frontMatter.layout
     })
-    
+
     #parseRouter() {
         for (let keyword in this.#routerKeywords) {
             if (!(this.#rule.includes(keyword))) continue
             let key = this.#routerKeywords[keyword]
             if (this.#staticKeywords.includes(keyword)) {
                 this.#rule = this.#rule.replaceAll(keyword, key)
-            }else{
-                this.#rule = this.#rule.replaceAll(keyword, "{slug}")
-                this.#slugInRule.push(key)
+            } else {
+                this.#rule = this.#rule.replaceAll(keyword, `<${key}>`)
             }
         }
         if (sep != "/") this.#rule = this.#rule.replaceAll("/", sep)
     }
+
     format() {
-        let _rule = this.#rule.split("{slug}")
-        _rule = _rule.map(i => i!="" ? dayjs(this.#layoutContext.frontMatter.date).format(i):"")
-        let result = _rule.join("{slug}")
-        for (let slug of this.#slugInRule) {
-            result = result.replace("{slug}", slug)
+        let dayjsRules = this.#rule.split(/<[^>]*>/g).filter(rule=>rule!='')
+        dayjsRules = dayjsRules.map(i => i != "" ? dayjs(this.#layoutContext.frontMatter.date).format(i) : "")
+        let buildInRules = (this.#rule.match(/<[^>]*>/g) || []).filter(rule=>rule!='')
+        buildInRules = buildInRules.map(rule => rule.replaceAll("<", "").replaceAll(">", ""))
+        let firstRules: string[], secondRules: string[]
+        if (/<[^>]*>/.exec(this.#rule)?.index != 0) {
+            firstRules = dayjsRules
+            secondRules = buildInRules
+        } else {
+            firstRules = buildInRules
+            secondRules = dayjsRules
         }
+        let _rules: string[] = []
+        firstRules.forEach((rule, index) => {
+            _rules.push(rule)
+            let _rule_second = secondRules[index]
+            if (_rule_second) _rules.push(_rule_second)
+        })
+        let result = _rules.join("")
         return `${result}${this.#withIndex ? `${sep}index.` : "."}`
     }
 }
