c994f89c92a47ed9e04cbf84473e225d0763da02
MaxChang3 <maxcfoo@gmail.com>
2022-02-06 20:15:46 +0800

    style(core): add comments todo

--- packages/core/src/maxwell.ts
+++ packages/core/src/maxwell.ts
@@ -35,21 +35,26 @@ class maxwell implements maxwellCore {
     async* render() {
         if (!(this.renderer)) throw new Error("renderer not init")
         for await (let layoutContext of getFilesContext(this.context)) {
+            //<Filter Plugin> todo:1 before_content_render
             layoutContext.content = await this.renderer.markdown.render(layoutContext, this.context)
+            //<Filter Plugin> todo:2 after_content_render
             let _context: context = {
                 config: this.context.config,
                 layoutContext
             }
+            //<Filter Plugin> todo3: before_layout_render
             layoutContext.content = await this.renderer.template.render({
                 filename: `${layoutContext.frontMatter.layout}`,
-                path: this.context.config.directory.template
+                path: this.context.config.directory.template //<Router Plugin> todo1
             }, _context)
+             //<Filter Plugin> todo4: after_layout_render
             layoutContext.filename = `${layoutContext.filename}.${this.renderer.template.options?.output}`
             yield layoutContext
         }
     }
     async write(postGenerator: AsyncGenerator<layoutContext, void, unknown>) {
         for await (let layoutContext of postGenerator) {
+            //<Router Plugin> todo2
             let basepath = [
                 this.context.config.directory.public,
                 layoutContext.frontMatter.layout,
