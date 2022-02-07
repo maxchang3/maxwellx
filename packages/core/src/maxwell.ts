7fcab8e99460985529c61590e1f764fe497717b2
MaxChang3 <maxcfoo@gmail.com>
2022-02-07 17:35:34 +0800

    refactor(core): refact promiseAll using in render&write

--- packages/core/src/maxwell.ts
+++ packages/core/src/maxwell.ts
@@ -34,54 +34,57 @@ class maxwell implements maxwellCore {
             markdown: _markdown
         }
     }
+    async #promiseAllObjSquare(filesContext: filesContext, callback: (layout: string, file: string) => Promise<void>) {
+        return promiseAllObject(filesContext, async (layout) => {
+            promiseAllObject(filesContext[layout], (file) => callback(layout, file))
+        })
+    }
     async render() {
         if (!(this.renderer)) throw new Error("renderer not init")
         const { markdown, template } = this.renderer
         let filesContext = await getFilesContext(this.context)
-        await promiseAllObject(filesContext, async (layout) => {
-            promiseAllObject(filesContext[layout], async (file) => {
-                let pageContext = filesContext[layout][file]
-                //<Filter Plugin> todo:1 before_content_render
-                pageContext.content = await markdown.render(pageContext, this.context)
-                //<Filter Plugin> todo:2 after_content_render
-                let _context: context = {
-                    config: this.context.config,
-                    pageContext
-                }
-                //<Filter Plugin> todo3: before_layout_render
-                pageContext.content = await template.render({
-                    filename: `${pageContext.frontMatter.layout}`,
-                    path: this.context.config.directory.template
-                }, _context)
-                //<Filter Plugin> todo4: after_layout_render
-            })
+        await this.#promiseAllObjSquare(filesContext, async (layout, file) => {
+            let pageContext = filesContext[layout][file]
+            //<Filter Plugin> todo:1 before_content_render
+            pageContext.content = await markdown.render(pageContext, this.context)
+            //<Filter Plugin> todo:2 after_content_render
+            let _context: context = {
+                config: this.context.config,
+                pageContext
+            }
+            //<Filter Plugin> todo3: before_layout_render
+            pageContext.content = await template.render({
+                filename: `${pageContext.frontMatter.layout}`,
+                path: this.context.config.directory.template
+            }, _context)
+            //<Filter Plugin> todo4: after_layout_render
         })
+
+        console.log(filesContext)
         return filesContext
     }
     async write(filesContext: filesContext) {
         if (!(this.renderer)) throw new Error("renderer not init")
         const { template } = this.renderer
-        await promiseAllObject(filesContext, async (layout) => {
-            promiseAllObject(filesContext[layout], async (file) => {
-                let layoutRouter
-                let pageContext = filesContext[layout][file]
-                let _layout = pageContext.frontMatter.layout
-                if (!(Object.keys(this.context.config.url.router).includes(_layout))) {
-                    _layout = "*"
-                }
-                layoutRouter = new Router(
-                    this.context.config.url.router[_layout].rule,
-                    pageContext,
-                    this.context.config.url.router[_layout].withIndex
-                );
-                pageContext.filename = layoutRouter.format()
-                pageContext.filename += template.options?.output
-                let basepath = [
-                    this.context.config.directory.public,
-                    ...pageContext.filename.split(sep)
-                ]
-                writeFile(basepath, pageContext.content)
-            })
+        await this.#promiseAllObjSquare(filesContext, async (layout, file) => {
+            let layoutRouter
+            let pageContext = filesContext[layout][file]
+            let _layout = pageContext.frontMatter.layout
+            if (!(Object.keys(this.context.config.url.router).includes(_layout))) {
+                _layout = "*"
+            }
+            layoutRouter = new Router(
+                this.context.config.url.router[_layout].rule,
+                pageContext,
+                this.context.config.url.router[_layout].withIndex
+            );
+            pageContext.filename = layoutRouter.format()
+            pageContext.filename += template.options?.output
+            let basepath = [
+                this.context.config.directory.public,
+                ...pageContext.filename.split(sep)
+            ]
+            writeFile(basepath, pageContext.content)
         })
 
     }
