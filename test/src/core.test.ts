4fb9db7f0dc3500dca7e2c533f7723f8afe150c3
MaxChang3 <maxcfoo@gmail.com>
2022-02-07 16:15:51 +0800

    test(core): update test code

--- test/src/core.test.ts
+++ test/src/core.test.ts
@@ -2,8 +2,12 @@ import { maxwell } from "@maxwellx/core";
 // import { getPostFilesContent } from "@maxwellx/layout";
 const core = new maxwell();
 await core.init();
-let renderList = core.render()
-core.write(renderList)
+let start = performance.now()
+let files = await core.render()
+await core.write(files)
+let end = performance.now()
+console.log(end - start)
+// core.write(renderList)
 // for await(let content of getPostFilesContent(core.context)){
 //     console.log(content)
 // }
