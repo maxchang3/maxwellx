7d6adfac75369e744f5c37bede2391b8da10e2c0
MaxChang3 <maxcfoo@gmail.com>
2022-02-07 20:24:02 +0800

    feat(core): update test

--- test/src/core.test.ts
+++ test/src/core.test.ts
@@ -1,10 +1,10 @@
 import { maxwell } from "@maxwellx/core";
 // import { getPostFilesContent } from "@maxwellx/layout";
 const core = new maxwell();
-await core.init();
 let start = performance.now()
-let files = await core.render()
-await core.write(files)
+await core.init();
+await core.render()
+await core.write()
 let end = performance.now()
 console.log(end - start)
 // core.write(renderList)
