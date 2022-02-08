02c089cbe4fa9eafa54ec685ab5e49fbb0d5807a
MaxChang3 <maxcfoo@gmail.com>
2022-02-08 23:30:45 +0800

    fix(context): fix error reports logical

--- packages/context/src/logger.ts
+++ packages/context/src/logger.ts
@@ -4,5 +4,11 @@ export class logger {
   static sucesss = consola.success
   static log = consola.log
   static info = consola.info
-  static error = (errorMessage?: string) => consola.error(new Error(errorMessage))
+  static error = (errorMessage?: string) => {
+    if (typeof errorMessage === "object") {
+      consola.error(errorMessage)
+    } else {
+      consola.error(new Error(errorMessage))
+    }
+  }
 }
