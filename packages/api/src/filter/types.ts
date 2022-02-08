cbe10b8dc8b2288acfc5164aae5cf1385129e7ea
MaxChang3 <maxcfoo@gmail.com>
2022-02-08 18:35:24 +0800

    feat(api/filter): remove typs

--- packages/api/src/filter/types.ts
+++ packages/api/src/filter/types.ts
@@ -1,10 +1,8 @@
 import type { pageContext } from "@maxwellx/layout"
 
 type filter = "before_content_render" |
-    "after_content_render" |
-    "before_layout_render" |
-    "after_layout_render"
+    "after_content_render" 
 
-type filterFunc = (pageContext: pageContext) => Promise<pageContext>
+type filterFunc = (pageContext: pageContext) => pageContext
 
 export { filter, filterFunc }
