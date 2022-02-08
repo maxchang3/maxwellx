5084d61489ce598ff4c1c64c02aea5d8b33f024b
MaxChang3 <maxcfoo@gmail.com>
2022-02-08 21:42:22 +0800

    feat(plugin/renderer-markdown-it): toc support

--- packages/plugins/renderer-markdown-it/src/index.ts
+++ packages/plugins/renderer-markdown-it/src/index.ts
@@ -1,11 +1,22 @@
 import { __dirname } from "@maxwellx/context"
 import { Renderer, withContent, definePlugin } from "@maxwellx/api";
 import MarkdownIt from "markdown-it";
-
+import anchor from "markdown-it-anchor";
+import toc from "markdown-it-toc-done-right";
 const md = MarkdownIt()
-
-const markdownitRenderer = new Renderer<withContent>(async(pageContext,context,options)=>{
+let tocData: string = ""
+md.use(anchor, { permalink: false }).use(toc, {
+    callback: (html) => {
+        tocData = html
+    },
+    containerId: "toc-container",
+    listClass: "toc-list",
+    itemClass: "toc-item",
+    linkClass: "toc-link",
+})
+const markdownitRenderer = new Renderer<withContent>(async (pageContext, context, options) => {
     pageContext.content = md.render(pageContext.content)
+    pageContext.data.toc = tocData
     return pageContext
 })
 
