7cb46f7a3c6e349a9369215a7179a019665bbd23
MaxChang3 <maxcfoo@gmail.com>
2022-02-09 00:16:52 +0800

    test(test): test for cli

--- test/maxwell.config.js
+++ test/maxwell.config.js
@@ -1,33 +1,32 @@
 import { returnConfig } from "@maxwellx/context";
-
 export default returnConfig({
-    site: {
-        title: "Maxwell Static Site Generator",
-        author: "Max",
-    },
-    url: {
-        url: "",
-        root: "/test/public/",
-        router: {
-            "post": {
-                rule: ":year/:month/:day/:filename",
-                withIndex: true
-            },
-            "*": {
-                rule: ":layout/:filename",
-                withIndex: false
-            }
-        }
-    },
-    directory: {
-        source: "source",
-        public: "public",
-        template: "theme"
-    },
-    template: "default",
-    plugins: [
-        "@maxwellx/renderer-eta",
-        "@maxwellx/renderer-markdown-it",
-        "@maxwellx/generator-basic",
-    ]
+	"site": {
+		"title": "Maxwell Blog",
+		"author": "Maxwell"
+	},
+	"url": {
+		"url": "",
+		"root": "/",
+		"router": {
+			"post": {
+				"rule": ":year/:month/:day/:filename",
+				"withIndex": true
+			},
+			"*": {
+				"rule": ":layout/:filename",
+				"withIndex": true
+			}
+		}
+	},
+	"directory": {
+		"source": "source",
+		"public": "public",
+		"template": "theme"
+	},
+	"template": "default",
+	"plugins": [
+		"@maxwellx/renderer-eta",
+		"@maxwellx/renderer-markdown-it",
+		"@maxwellx/generator-basic"
+	]
 })
