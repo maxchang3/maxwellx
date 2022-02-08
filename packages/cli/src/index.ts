7d86727bc0c66d2710be7ce16f1042081974fbf0
MaxChang3 <maxcfoo@gmail.com>
2022-02-09 01:14:12 +0800

    fix(cli): remove useless code

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -30,16 +30,6 @@ program
     .description('cli for maxwellx static site generator')
     .version("1.0.0");
 
-program.command('split')
-    .description('Split a string into substrings and display as an array')
-    .argument('<string>', 'string to split')
-    .option('--first', 'display just the first substring')
-    .option('-s, --separator <char>', 'separator character', ',')
-    .action((str, options) => {
-        const limit = options.first ? 1 : undefined;
-        console.log(str.split(options.separator, limit));
-    });
-
 program.command('generate')
     .alias('g')
     .description('generate site from your source file')
