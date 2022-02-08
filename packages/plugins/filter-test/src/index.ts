ab49b92d570b6430baedd704c4b670d11a70aa06
MaxChang3 <maxcfoo@gmail.com>
2022-02-08 18:35:40 +0800

    feat(plugin): add filter test plugin

---
+++ packages/plugins/filter-test/src/index.ts
@@ -0,0 +1,17 @@
+import { definePlugin, Filter } from "@maxwellx/api";
+
+const testFilter1 = new Filter("before_content_render", (pageContext) => {
+    pageContext.content+="##before_content_render"
+    return pageContext
+})
+
+const testFilter2 = new Filter("after_content_render", (pageContext) => {
+    pageContext.content+="##after_content_render"
+    return pageContext
+})
+
+
+export default definePlugin([
+    testFilter1,
+    testFilter2
+])
