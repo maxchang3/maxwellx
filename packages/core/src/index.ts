45cac8dba7d69bd3b140b2481bc18e7c7b5d77c3
MaxChang3 <maxcfoo@gmail.com>
2022-02-15 20:20:13 +0800

    refactor(core): add export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,3 @@
-export {maxwell} from './maxwell.js'
+export { maxwell } from './maxwell.js'
+export { logger, defaultConfig, writeFile } from "@maxwellx/context"
+export type { configure } from "@maxwellx/context"
